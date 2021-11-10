bf70d1e0b7e3bd9864de69f35cdc5f20d822033a
Jason Desrosiers <jdesrosi@gmail.com>
2021-11-09 20:17:42 -0800

    Initial implementation

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,28 @@
+import type { Core, Schema, SchemaDocument, SchemaObject, InvalidSchemaError } from "@hyperjump/json-schema-core";
+
+
 export type JsonSchemaBundler = {
+  add: typeof Schema.add;
+  get: typeof Schema.get;
+  bundle: (schemaDoc: SchemaDocument) => Promise<SchemaObject>;
+  setMetaOutputFormat: typeof Core.setMetaOutputFormat;
+  setShouldMetaValidate: typeof Core.setShouldMetaValidate;
+  FLAG: typeof Core.FLAG;
+  BASIC: typeof Core.BASIC;
+  DETAILED: typeof Core.DETAILED;
+  VERBOSE: typeof Core.VERBOSE;
+  InvalidSchemaError: typeof InvalidSchemaError;
 };
+
+export const add: JsonSchemaBundler["add"];
+export const get: JsonSchemaBundler["get"];
+export const bundle: JsonSchemaBundler["bundle"];
+export const setMetaOutputFormat: JsonSchemaBundler["setMetaOutputFormat"];
+export const setShouldMetaValidate: JsonSchemaBundler["setShouldMetaValidate"];
+export const FLAG: JsonSchemaBundler["FLAG"];
+export const BASIC: JsonSchemaBundler["BASIC"];
+export const DETAILED: JsonSchemaBundler["DETAILED"];
+export const VERBOSE: JsonSchemaBundler["VERBOSE"];
 
 export * from "@hyperjump/json-schema";
 
